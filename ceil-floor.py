56eb75b6ae08e1b9fcfbf24be1f33569442f8f10
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2021-03-19 19:55:48 -0500

    Create ceil-floor.py

---
+++ ceil-floor.py
@@ -0,0 +1,40 @@
+#Write a script which can ceil and floor given values without using any python built-in functions
+#Given x and y,find the ceiling value of x/y without using ceiling function
+#ceil value =(x+y-1)y
+x=5
+y=4
+val=(x+y-1)/y
+print(val)
+
+
+
+#Write a script which can ceil and floor given values without using any python built-in functions
+#Given a and b,find the floor value of x/y without using ceiling function
+a=5
+b=3
+val =(a//b)+((a%b)!=0)
+print("The ceil value of 5//3:",val)
+
+
+#Write a program that ask user their name 
+
+user_name=int(input("What is your name?"))
+
+if (user_name is Azeez Olanrewaju):
+    print("That is a popular name:")
+
+elif user_name is "John Cleese":
+      print("I feel greet about you:")
+
+elif user_name is "Michael Palin":
+     print("I am proud of you:")
+else:
+     print("You have a nice name:")    
+    
+
+count = 0
+while count < 5:
+    print(count, " is less than 5")
+    count = count + 1
+else:
+    print(count, " is not less than 5")
